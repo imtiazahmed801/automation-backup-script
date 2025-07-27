a77e085cd2b649de916f5d1dbb4d08d6e3f9254c
imtiaz <imtiazya@protonmail.com>
2025-07-27 20:57:31 +0100

    Backup automation script

---
+++ backup.py
@@ -0,0 +1,24 @@
+import os
+import shutil
+import datetime
+import schedule 
+import time 
+
+source_dir = "/home/imti/Desktop/python-projects"
+destination_dir = "/home/imti/Desktop/backups"
+
+def copy_folder_to_directory(source, dest):
+    today = datetime.date.today()
+    dest_dir = os.path.join(dest, str(today))
+    
+    try:
+        shutil.copytree(source, dest_dir)
+        print(f"Folder copied to: {dest_dir}")
+    except FileExistsError:
+        print(f"Folder already exists in: {dest}")
+
+schedule.every().day.at("20:48").do(lambda: copy_folder_to_directory(source_dir, destination_dir))
+
+while True:
+    schedule.run_pending()
+    time.sleep(60)
